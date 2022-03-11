27e3b2b24d7e5dbe5db7fb53cd3baa7b3113a1e0
Casio271 <michal.k7w@gmail.com>
2022-03-11 21:07:08 +0000

    discrete qubo

--- extended_version.py
+++ extended_version.py
@@ -1,4 +1,5 @@
 # ------ Import necessary packages ----
+from cProfile import label
 from email import iterators
 import math
 import random
@@ -13,10 +14,12 @@ matplotlib.use("agg")
 from matplotlib import pyplot as plt
 
 import dimod
+import dwavebinarycsp
 import dwave.inspector
 from minorminer import find_embedding
 from dwave.embedding import embed_ising
 from dwave.system import LeapHybridSampler
+from dwave.system import LeapHybridDQMSampler
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite, LazyFixedEmbeddingComposite, FixedEmbeddingComposite
 
@@ -31,6 +34,7 @@ def define_dirs(n, k, dim, ord, g, custom,type):
     dirs = {
         "name"              : ''.join([                   str(n), "_graph_snn", "_k", str(k), "_dim", str(dim), "_g", str(g), type_names[type], str(ord)]                       ),
         "graph_in"          : ''.join(["./DatasetsIn/"  , str(n), "_graph_snn", "_k", str(k), "_dim", str(dim),               type_names[type], str(ord),         ".gexf"       ]),
+        "graph_in_csv"      : ''.join(["./DatasetsIn/"  , str(n), "_graph_snn", "_k", str(k), "_dim", str(dim),               type_names[type], str(ord),         ".csv"       ]),
         "graph_out"         : ''.join(["./DatasetsOut/" , str(n), "_graph_snn", "_k", str(k), "_dim", str(dim), "_g", str(g), type_names[type], str(ord), custom, "_out.gexf"   ]),
         "graph_to_compare"  : ''.join(["./DatasetsOut/" , str(n), "_graph_snn", "_k", str(k), "_dim", str(dim),               type_names[type], str(ord), custom, ".gexf"       ]),
         "img_in"            : ''.join(["./PlotsIn/"     , str(n), "_graph_snn", "_k", str(k), "_dim", str(dim), "_g", str(g), type_names[type], str(ord), custom, ".png"        ]),
@@ -41,6 +45,16 @@ def define_dirs(n, k, dim, ord, g, custom,type):
 
 def create_graph(dirs):
     G = nx.read_gexf(dirs["graph_in"])
+    pos = nx.spring_layout(G)
+    return G, pos
+
+def create_graph_csv(dirs):
+    input_data = pd.read_csv(dirs["graph_in_csv"], header=0, usecols={1,2,3})
+    records = input_data.to_records(index=False)
+    result = list(records)
+    len(result)
+    G = nx.Graph()
+    G.add_weighted_edges_from(result)
     pos = nx.spring_layout(G)
     return G, pos
 
@@ -113,7 +127,7 @@ def open_inspector(G):
     response = sampler.sample_qubo(Q, label=name, chain_strength=chain_strength, num_reads=num_reads)    
     dwave.inspector.show(response) # , block='never'
 
-def clustering(G, iteration, dirs, name, solver, gamma_factor, gamma_type, color, terminate_on):
+def clustering_recur2(G, iteration, dirs, name, solver, gamma_factor, gamma_type, color, terminate_on):
 
     name_spec = ''.join([name, "_", solver]) 
     name_spec_emb = "./Embedding/" + name_spec + str(iteration) + ".json"
@@ -253,6 +267,32 @@ def clustering(G, iteration, dirs, name, solver, gamma_factor, gamma_type, color
     
     return
 
+def clustering_discrete(G):
+    nodes = G.nodes
+    edges = G.edges
+    clusters = [0,1,2,3,4]
+
+    gamma = 0.005
+
+    dqm = dimod.DiscreteQuadraticModel()
+    for node in nodes:
+        dqm.add_variable(5, label=node)
+    
+    for node in nodes:
+        dqm.set_linear(node, [gamma*(1-len(G.nodes)/3) for cluster in clusters])
+
+    for i, j in combinations(nodes, 2):
+        dqm.set_quadratic(i, j, {(cluster, cluster) : 2*gamma for cluster in clusters})
+
+    for u, v in edges:
+        dqm.set_quadratic(u, v, {(cluster, cluster) : -2*G.get_edge_data(u, v)["weight"] for cluster in clusters})
+        dqm.set_linear(u, [G.get_edge_data(u, v)["weight"] for cluster in clusters])
+        dqm.set_linear(v, [G.get_edge_data(u, v)["weight"] for cluster in clusters])
+
+    sampleset = LeapHybridDQMSampler().sample_dqm(dqm, label='DQM - scRAN-seq') 
+    print("Energy: {}\nSolution: {}".format(sampleset.first.energy, sampleset.first.sample)) 
+    return sampleset
+
 solvers = {
     "h"     : "hybrid",
     "fe"    : "fixed_embedding",
@@ -266,10 +306,10 @@ ord = 15    # maximum order of node degree when "trimmed" mode is enabled
 dim = 15    # number of dimensions used for SNN
 type = 1    #["_", "_trimmed_", "_negedges_", "_trimmed_negedges_"], where "_" -> unaltered SNN output
 color = 0   # initial value of clusters coloring
-gamma_factor = 0.05      # gamma_factor, weights the clusters' sizes constraint
+gamma_factor = 0.05                 # gamma_factor, weights the clusters' sizes constraint
 gamma_by = "by_edges" 
-custom = "data_11_03" # additional metadata for file names
-terminate_on = "min_size"  # other options: "conf"
+custom = "data_11_03_5clusters3"    # additional metadata for file names
+terminate_on = "min_size"           # other options: "conf"
 
 # definition of files locations
 dirs = define_dirs(n, k, dim, ord, gamma_factor, custom, type)
@@ -277,16 +317,21 @@ name = dirs["name"]
 
 # cration of the input graph
 G, pos = create_graph(dirs)
-# plot_and_save_graph_in(G, pos, dirs)
+# G, pos = create_graph_csv(dirs)
+plot_and_save_graph_in(G, pos, dirs)
 
-iteration = 1
-clustering(G, iteration, dirs, name, solver, gamma_factor, gamma_by, color, terminate_on)
+sampleset = clustering_discrete(G)
+        
+# Adjust the next line if using a different map
+plt.cla()
+nx.draw(G, pos=pos, with_labels=False, node_color=list(sampleset.first.sample.values()), node_size=10, cmap=plt.cm.rainbow)                 
+plt.savefig(dirs["img_out"], bbox_inches='tight')
 
-plot_and_save_graph_out(G, pos, dirs)
+# iteration = 1
+# clustering_recur2(G, iteration, dirs, name, solver, gamma_factor, gamma_by, color, terminate_on)
 
-nx.write_gexf(G, dirs["graph_to_compare"])
+# plot_and_save_graph_out(G, pos, dirs)
+
+# nx.write_gexf(G, dirs["graph_to_compare"])
 
 # open_inspector(G)
-
-
-
